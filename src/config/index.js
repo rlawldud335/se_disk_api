6f5a15147b28ccfcb5996b4b65fe205006ad834a
rlawldud335 <rlawldud335@naver.com>
2021-10-31 04:06:39 +0900

    config2

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,3 @@
-import dotenv from 'dotenv';
-
-const envFound = dotenv.config();
-if (envFound.error) {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
 
 export default {
     port: parseInt(process.env.PORT, 10),
